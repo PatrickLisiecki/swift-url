8019ebbe7aa3fc03ff41b1481e8a3df3a582dc20
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-08-21 13:02:34 -0400

    Added colors and font for config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	darkMode: "class",
 	theme: {
-		extend: {}
+		fontFamily: {
+			raleway: ["Raleway", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				"dark-body": "#1c1c1c",
+				"dark-text": "#ededed",
+				"dark-text-secondary": "#565656",
+				"dark-border": "#3e3e3e",
+				"dark-border-hover": "#656565",
+				"dark-bg": "#232323",
+				accent: "#FFA500",
+			},
+		},
 	},
-	plugins: []
+	plugins: [],
 };
