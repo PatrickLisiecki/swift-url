bbed2c03edaa38a26e33213bef36cb77959cb0c9
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-08-21 13:26:18 -0400

    Added default and signup route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./assets/index.css";
+import Root from "./Root.jsx";
+import "./assets/styles/index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Pages
+import SignUp from "./pages/signup/SignUp";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+	},
+	{
+		path: "/signup",
+		element: <SignUp />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
